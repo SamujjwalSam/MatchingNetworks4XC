3ea9d1a317aebb9bd3287c16496419178e1824fe
Samujjwal <cs16resch01001@iith.ac.in>
2019-05-17 09:50:45 +0530

    added code to get username in config.

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 # coding=utf-8
 # !/usr/bin/python3.6  # Please use python 3.6
 """
-__synopsis__    : Config file for global variables.
+__synopsis__    : Contains system configuration details and other global variables.
 
-__description__ : We can print the configuration on every run and get the hyper-parameter configuration for each run.
+__description__ : Benefit: We can print the configuration on every run and get the hyper-parameter configuration for each run.
 __project__     : MNXC
 __author__      : Samujjwal Ghosh <cs16resch01001@iith.ac.in>
 __version__     : ": 0.1 "
@@ -13,12 +13,12 @@ __license__     : "This source code is licensed under the MIT-style license foun
                   directory of this source tree."
 
 __classes__     : config
-    
+
 __variables__   : configuration, seed, platform
-    
+
 __methods__     :
 
-__last_modified__: 
+__last_modified__:
 """
 
 import json
@@ -30,19 +30,20 @@ seed = 0
 global configuration
 configuration = {
     "data": {
-        "dataset_name": "Wiki10-31K",
+        "dataset_name": "Wiki10-31K_onehot",
         "val_split": 0.1,
         "test_split": 0.3,
         "show_stat": False
     },
 
     "xc_datasets": {
-        "AmazonCat-14K": "json",
-        "Amazon-3M": "json",
-        "Delicious-T140": "html",
         "Wiki10-31K": "html",
         "Wiki10-31K_onehot": "html",
         "Wiki10-31K_fixed5": "html",
+        "Wiki10-31K_pointer": "html",
+        "AmazonCat-14K": "json",
+        "Amazon-3M": "json",
+        "Delicious-T140": "html",
         "AmazonCat-13K": "txt",
         "Amazon-670K": "txt",
         "EurLex": "uniq"
@@ -66,17 +67,17 @@ configuration = {
         "padding": 1,
         "context": 10,
         "classify_count": 0,
-        "fce": True,
+        "fce": False,
         "optimizer": {
             "optimizer_type": "adam",
-            "learning_rate": 1,
-            "lr_decay": 1e-6,
-            "weight_decay": 1e-4,
-            "momentum":0.9,
-            "dampening":0.9,
-            "alpha":0.99,
-            "rho":0.9,
-            "centered":False
+            "learning_rate": 3e-4,
+            "lr_decay": 0,
+            "weight_decay": 0,
+            "momentum": 0.9,
+            "dampening": 0.9,
+            "alpha": 0.99,
+            "rho": 0.9,
+            "centered": False
         },
         "view_grads": False,
         "view_train_precision": True
@@ -100,10 +101,10 @@ configuration = {
     "sampling": {
         "num_epochs": 50,
         "num_train_epoch": 5,
-        "batch_size": 64,
-        "categories_per_batch": 10,
-        "supports_per_category": 5,
-        "targets_per_category": 5
+        "batch_size": 2,
+        "categories_per_batch": 3,
+        "supports_per_category": 2,
+        "targets_per_category": 2
     },
 
     "prep_vecs": {
@@ -113,15 +114,16 @@ configuration = {
         "min_count": 1,
         "negative": 10,
         "num_chunks": 10,
-        "vectorizer": "doc2vec",
+        "vectorizer": "word2vec",
         "sample_repeat_mode": "append",
         "input_size": 300,
-        "tfidf_avg": False
+        "tfidf_avg": False,
+        "idf" : True
     },
 
     "text_process": {
         "encoding": 'latin-1',
-        "sents_chunk_mode": "word_sum",
+        "sents_chunk_mode": "word_avg",
         "workers": 5
     },
 
@@ -131,14 +133,20 @@ configuration = {
 
         "pretrain_dir": {
             "Windows": "D:\\Datasets\\Extreme Classification",
-            "Linux": "/raid/ravi/pretrain",  ## Code path: /home/cs14resch11001/codes/MNXC
-            "OSX": "/home/cs16resch01001/datasets/Extreme Classification"
+            "OSX": "/home/cs16resch01001/datasets/Extreme Classification",
+            "Linux": {
+                "sam":"/media/sam/Data/Datasets/pretrain",  ## Code path: /home/cs14resch11001/codes/MNXC
+                "cs14resch11001":"/raid/ravi/pretrain"
+            }
         },
 
         "dataset_dir": {
             "Windows": "D:\\Datasets\\Extreme Classification",
-            "Linux": "/raid/ravi/Datasets/Extreme Classification",
-            "OSX": "/home/cs16resch01001/datasets/Extreme Classification"
+            "OSX": "/home/cs16resch01001/datasets/Extreme Classification",
+            "Linux": {
+                "sam":"/media/sam/Data/Datasets/Extreme Classification",
+                "cs14resch11001":"/raid/ravi/Datasets/Extreme Classification"
+            }
         }
     },
 
@@ -161,9 +169,7 @@ configuration = {
 
 
 class Config(object):
-    """
-        Details of the class.
-    """
+    """ Contains all configuration details of the project. """
 
     def __init__(self):
         super(Config, self).__init__()
@@ -178,16 +184,14 @@ class Config(object):
         return self.configuration
 
     def print_config(self, indent=4, sort=True):
-        """
-        Prints the config.
-        """
+        """ Prints the config. """
         logger.info("[{}] : {}".format("Configuration", json.dumps(self.configuration, indent=indent, sort_keys=sort)))
 
-    def get_platform(self):
-        """
-        Returns dataset path based on OS.
+    @staticmethod
+    def get_platform():
+        """ Returns dataset path based on OS.
 
-        :return: Returns dataset path based on OS.
+        :return: str
         """
         import platform
 
@@ -198,12 +202,32 @@ class Config(object):
         else:  ## OS X returns name 'Darwin'
             return "OSX"
 
+    @staticmethod
+    def get_username():
+        """
+        :returns the current username.
+
+        :return: string
+        """
+        try:
+            import os, pwd
+            username = pwd.getpwuid(os.getuid()).pw_name
+        except Exception as e:
+            import getpass
+            username = getpass.getuser()
+        # finally:
+        #     username = os.environ.get('USER')
+
+        return username
+
 
 config_cls = Config()
 config_cls.print_config()
 
 global platform
 platform = config_cls.get_platform()
+global username
+username = config_cls.get_username()
 
 
 def main():
